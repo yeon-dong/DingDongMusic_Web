d857be2178dd1494501d8273252417735ee71746
yeon-dong <jae073@naver.com>
2024-09-12 09:29:25 +0900

    Noto Sans 폰트 설정 완료

--- dingdong-music/src/App.jsx
+++ dingdong-music/src/App.jsx
@@ -1,9 +1,11 @@
 import Header from "./components/Header/header";
+import MainBox from "./components/MainBox/MainBox";
 
 function App() {
   return (
     <>
       <Header></Header>
+      <MainBox></MainBox>
     </>
   );
 }
---
+++ dingdong-music/src/components/MainBox/MainBox.style.js
@@ -0,0 +1,13 @@
+import { styled } from "styled-components";
+
+export const Container = styled.div`
+  background-color: var(--primary-color);
+  width: 1092px;
+  height: 623px;
+`;
+
+export const MainText = styled.h1`
+  color: white;
+  font-size: 30px;
+  font-weight: bold;
+`;
