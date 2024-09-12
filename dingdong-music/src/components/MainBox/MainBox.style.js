add7ff82f947151b3a62d85eefe07f4a72446d3e
yeon-dong <jae073@naver.com>
2024-09-12 10:30:49 +0900

    data.js 추가

--- dingdong-music/src/components/MainBox/MainBox.style.js
+++ dingdong-music/src/components/MainBox/MainBox.style.js
@@ -10,4 +10,48 @@ export const MainText = styled.h1`
   color: white;
   font-size: 30px;
   font-weight: bold;
+  padding-top: 18px;
+  margin-left: 32px;
+`;
+
+export const MainText2 = styled.h1`
+  color: white;
+  font-size: 30px;
+  font-weight: bold;
+  margin-top: 22px;
+  margin-left: 32px;
+`;
+
+export const MusicContainer = styled.div`
+  margin-top: 8px;
+  margin-left: 12px;
+`;
+
+export const MusicInfoContainer = styled.div`
+  width: 180px;
+  height: 235px;
+`;
+
+export const MusicImg = styled.img`
+  width: 160px;
+  height: 160px;
+  border-radius: 100%;
+  margin-top: 10px;
+  margin-left: 11px;
+`;
+
+export const MusicMainText = styled.div`
+  font-size: 14px;
+  font-weight: bold;
+  color: white;
+  margin-top: 11px;
+  margin-left: 11px;
+`;
+
+export const MusicSubText = styled.div`
+  font-size: 14px;
+  font-weight: regular;
+  color: var(--primary-color2);
+  margin-top: 10px;
+  margin-left: 11px;
 `;
---
+++ dingdong-music/src/data/data.js
@@ -0,0 +1,40 @@
+const musics = [
+  {
+    id: 1,
+    musicName: "Ditto",
+    artistName: "NewJeans",
+    musicImgSrc: "newjeans.jpg",
+    price: 15000,
+    description:
+      "Stay in the middle Like you a little Don't want no riddle 말해줘 say it back, oh, say it ditto 아침은 너무 멀어 so say it ditto",
+  },
+  {
+    id: 2,
+    musicName: "한 개도 몰라",
+    artistName: "Leellamarz",
+    musicImgSrc: "한개도몰라.jpg",
+    price: 15000,
+    description:
+      "난 너의 마음을 한 개도 몰라 두 개는 더 몰라 세 번째에는 떠올랐지 너가 우는 얼굴 말이야...",
+  },
+  {
+    id: 3,
+    musicName: "グッバイバイ / Good bye-bye",
+    artistName: "Ai Tomioka(토미오카 아이/冨岡 愛)",
+    musicImgSrc: "goodbye-bye.jpg",
+    price: 15000,
+    description:
+      "Be by your side 会いたいわ あなたに それだけが言えなかった I wanna say あなたのせいで 鼓動が 今日も高まってるの",
+  },
+  {
+    id: 4,
+    musicName: "고민중독",
+    artistName: "QWER",
+    musicImgSrc: "고민중독.jpg",
+    price: 15000,
+    description:
+      "쏟아지는 맘을 멈출 수가 없을까? 너의 작은 인사 한마디에 요란해져서 네 맘의 비밀번호 눌러 열고 싶지만 너를 고민고민해도 좋은 걸 어쩌니",
+  },
+];
+
+export default musics;
