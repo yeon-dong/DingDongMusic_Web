0611e83e6928ae804d318da71091eee16951f3d5
ektto1041 <53224839+ektto1041@users.noreply.github.com>
2024-09-12 17:28:32 +0900

    feat: SearchBox 구현

---
+++ dingdong-music/src/components/SearchBox/SearchBox.jsx
@@ -0,0 +1,58 @@
+import { useMemo, useState } from "react";
+import {
+  Container,
+  MusicImg,
+  MusicImgContainer,
+  MusicInfoContainer,
+  MusicMainText,
+  MusicPlayBtn,
+  MusicPlayIcon,
+  MusicSubText,
+  SearchBoxContent,
+  SearchBoxHeading,
+} from "./SearchBox.style";
+import musics from "../../data/data";
+
+const SearchBox = () => {
+  const [data, setData] = useState(musics);
+
+  const { pathname } = window.location;
+  const keyword = decodeURI(pathname.split("/")[2]);
+  console.log(keyword);
+
+  const searchList = useMemo(() => {
+    const regex = new RegExp(keyword, "i"); // 'i' 플래그는 대소문자 구분을 하지 않음
+
+    const list = [];
+
+    data.forEach((music) => {
+      const containsKeyword = regex.test(music.musicName);
+
+      if (containsKeyword) list.push(music);
+    });
+
+    return list;
+  }, [data, keyword]);
+
+  return (
+    <Container>
+      <SearchBoxHeading>검색 결과</SearchBoxHeading>
+      <SearchBoxContent>
+        {searchList.map((music, i) => (
+          <MusicInfoContainer>
+            <MusicImgContainer>
+              <MusicImg src={`/images/${music.musicImgSrc}`} />
+              <MusicPlayBtn>
+                <MusicPlayIcon src="/images/Vector.svg" />
+              </MusicPlayBtn>
+            </MusicImgContainer>
+            <MusicMainText>{music.musicName}</MusicMainText>
+            <MusicSubText>{music.artistName}</MusicSubText>
+          </MusicInfoContainer>
+        ))}
+      </SearchBoxContent>
+    </Container>
+  );
+};
+
+export default SearchBox;
