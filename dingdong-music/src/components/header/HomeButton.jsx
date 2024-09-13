6ef4d41c0ecd42b6212697d883a6529f9d91a4bc
ektto1041 <53224839+ektto1041@users.noreply.github.com>
2024-09-13 09:50:38 +0900

    feat: HomeButton 라우팅 기능 추가

--- dingdong-music/src/components/header/HomeButton.jsx
+++ dingdong-music/src/components/header/HomeButton.jsx
@@ -2,7 +2,7 @@ import { Container } from "./HomeButton.style";
 
 const HomeButton = () => {
   return (
-    <Container>
+    <Container to="/">
       <img src="/home_icon.svg" alt="Home Icon" />
     </Container>
   );
