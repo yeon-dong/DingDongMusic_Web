7082943895fc2da61fa153a110fd7cd064a3fab2
Song JiWoong <016vhsdkdl@gmail.com>
2024-09-16 01:03:58 +0900

    플레이리스트스토어 사용, 플레이리스트와 현재 재생음악 조건분기

--- dingdong-music/src/components/playbox/Playbox.jsx
+++ dingdong-music/src/components/playbox/Playbox.jsx
@@ -1,14 +1,73 @@
 import React, { useState } from "react";
 import { ProgressBar, ProgressCircle } from "./PlayBox.style";
 import "./PlayBox.css";
+import { useSelector } from "react-redux";
 
 function PlayBox() {
-  const playlistItems = useSelector((state) => state.playlistCart);
+  const [selectedIndex, setSelectedIndex] = useState(0);
+  const [selectedSong, setSelectedSong] = useState(null);
+  const { items, totalAlbums, totalAmount } = useSelector(
+    (state) => state.playlist
+  );
   const [isPushed, setIsPushed] = useState(false);
+
+  const playlistEmpty = () => (
+    <div
+      className="p-4 rounded-md mb-10 mobile-None"
+      style={{ background: "var(--primary-color)" }}
+    >
+      <h3 className="text-xl font-semibold mb-2 leading-7">
+        첫 번째 플레이리스트를 만드세요.
+      </h3>
+      <p className="text-sm text-gray-400 mb-4 leading-6">
+        어렵지 않아요. 저희가 도와드릴게요.
+      </p>
+      <button
+        onClick={() => {
+          console.log(items);
+        }}
+        className="bg-white text-black px-4 py-2 rounded-2xl font-bold hover:bg-gray-200"
+      >
+        플레이리스트 만들기
+      </button>
+    </div>
+  );
+  const playlistWithItem = () => (
+    <div className="w-full h-40 mb-5 overflow-auto scroll-hide mobile-None">
+      {items.map((item, i) => (
+        <div
+          key={i}
+          className={`p-1 rounded-md mb-2 mobile-None
+            ${selectedIndex === i ? "text-green-500" : ""}`}
+          style={{ background: "var(--primary-color)" }}
+          onClick={() => {
+            setSelectedIndex(i);
+            setSelectedSong(item);
+          }}
+        >
+          <div className="flex">
+            <div className="w-28 h-14">
+              <img
+                className="w-14 h-14 rounded-full"
+                src={`./images/${item.musicImgSrc}`}
+              ></img>
+            </div>
+            <div className="flex-col items-center py-1">
+              <h3 className="font-bold mb-2">{item.musicName}</h3>
+              <p className="text-sm" style={{ color: "#b3b3b3" }}>
+                {item.artistName}
+              </p>
+            </div>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
 
   return (
     <div
-      className="w-full h-full text-white p-4 rounded-md shadow-lg"
+      className="w-full h-full text-white p-4 rounded-md shadow-lg   overflow-y-auto scroll-hide
+"
       style={{ background: "var(--primary-color3)" }}
     >
       <div className="flex items-center justify-between mb-8 mobile-None">
@@ -20,43 +79,60 @@ function PlayBox() {
           <img src="./images/library_submenu.svg" alt="" />
         </div>
       </div>
+      {items.length > 0 ? playlistWithItem() : playlistEmpty()}
 
-      <div
-        className="p-4 rounded-md mb-10 mobile-None"
-        style={{ background: "var(--primary-color)" }}
-      >
-        <h3 className="text-xl font-semibold mb-2 leading-7">
-          첫 번째 플레이리스트를 만드세요.
-        </h3>
-        <p className="text-sm text-gray-400 mb-4 leading-6">
-          어렵지 않아요. 저희가 도와드릴게요.
-        </p>
-        <button className="bg-white text-black px-4 py-2 rounded-2xl font-bold hover:bg-gray-200">
-          플레이리스트 만들기
-        </button>
-      </div>
       <div className="h-28 mobile-None"></div>
-      <div className="h-60 flex flex-col p-4 pt-28 rounded-md relative mobile-On bg-custom-black">
-        <div className="absolute top-0 left-1/2 transform -translate-x-1/2 -translate-y-1/2 mobile-None">
-          <img src="./images/playlist_logo.svg" />
+      {selectedSong === null ? (
+        <div className="h-60 flex flex-col p-4 pt-28 rounded-md relative mobile-On bg-custom-black">
+          <div className="absolute top-0 left-1/2 transform -translate-x-1/2 -translate-y-1/2 mobile-None">
+            <img src="./images/playlist_logo.svg" />
+          </div>
+
+          <p className="text-base text-white font-bold ml-4 mb-10">
+            현재 플레이 중인 음악이 없습니다.
+          </p>
+
+          <div className="w-full flex flex-col">
+            <ProgressBar>
+              <ProgressCircle $isPushed={isPushed} />
+            </ProgressBar>
+            <button className="mx-auto">
+              <img src="./images/playbutton.svg"></img>
+            </button>
+          </div>
         </div>
-        <p className="text-base text-white font-bold ml-4 mb-10">
-          현재 플레이 중인 음악이 없습니다.
-        </p>
-        <div className="w-full flex flex-col">
-          <ProgressBar>
-            <ProgressCircle $isPushed={isPushed} />
-          </ProgressBar>
-          <button
-            className="mx-auto"
-            onClick={() => {
-              setIsPushed(!isPushed);
-            }}
-          >
-            <img src="./images/playbutton.svg"></img>
-          </button>
+      ) : (
+        <div className="h-60 flex flex-col p-4 pt-24 rounded-md relative mobile-On bg-custom-black">
+          <div className="absolute top-0 left-1/2 transform -translate-x-1/2 -translate-y-1/2 mobile-None">
+            <img
+              className="w-40 h-40 rounded-full"
+              src={`./images/${selectedSong.musicImgSrc}`}
+            />
+          </div>
+
+          <div className="text-base text-white  ml-4 mb-8">
+            <h3>{selectedSong.musicName}</h3>
+            <p style={{ color: "#b3b3b3" }}>{selectedSong.artistName}</p>
+          </div>
+          <div className="w-full flex flex-col">
+            <ProgressBar>
+              <ProgressCircle $isPushed={isPushed} />
+            </ProgressBar>
+            <button
+              className="mx-auto"
+              onClick={() => {
+                setIsPushed(!isPushed);
+              }}
+            >
+              {isPushed ? (
+                <img src="./images/pausebutton.svg"></img>
+              ) : (
+                <img src="./images/playbutton.svg"></img>
+              )}
+            </button>
+          </div>
         </div>
-      </div>
+      )}
     </div>
   );
 }
