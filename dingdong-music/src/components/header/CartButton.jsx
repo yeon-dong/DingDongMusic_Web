fdd4e1b5ffaf4a5ce9e978d014c7abac0bdf62d4
ektto1041 <53224839+ektto1041@users.noreply.github.com>
2024-09-19 10:22:21 +0900

    feat: 장바구니 버튼 클릭 시 이동하도록 수정

--- dingdong-music/src/components/header/CartButton.jsx
+++ dingdong-music/src/components/header/CartButton.jsx
@@ -1,10 +1,19 @@
+import { useCallback } from "react";
 import { Badge, CartIcon, Container } from "./CartButton.style";
 import { useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
 
 const CartButton = ({ color }) => {
+  const navigate = useNavigate();
+
   const { totalAlbums } = useSelector((state) => state.cart);
+
+  const handleClick = useCallback(() => {
+    navigate("/cart");
+  }, []);
+
   return (
-    <Container>
+    <Container onClick={handleClick}>
       <CartIcon>
         <img
           src={`/cart_icon${color === "white" ? "_white" : ""}.svg`}
