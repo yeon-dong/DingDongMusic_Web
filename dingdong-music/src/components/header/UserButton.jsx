2602b9b7371686f18be09f5805abeb89da1f0084
ektto1041 <53224839+ektto1041@users.noreply.github.com>
2024-09-23 17:24:16 +0900

    feat: map 라우팅 추가

--- dingdong-music/src/components/header/UserButton.jsx
+++ dingdong-music/src/components/header/UserButton.jsx
@@ -2,7 +2,7 @@ import { Container } from "./UserButton.style";
 
 const UserButton = () => {
   return (
-    <Container>
+    <Container to="/map">
       <img src="/images/avatar.svg" alt="User Icon" />
       {/* <img src="/user_icon.svg" alt="User Icon" /> */}
     </Container>
